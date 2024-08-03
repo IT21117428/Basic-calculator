0d3d489ccb81918fe6f39dc7eb40f27ab2920e53
kaveeja1999 <87494020+IT21117428@users.noreply.github.com>
2024-08-03 17:48:10 +0530

    finalize the programe
    create a do while loop to user want to restart the programme and otherwise user won't to restart the programme its display the have a good day massage

--- FirstProgram.java
+++ FirstProgram.java
@@ -2,39 +2,49 @@ import java.util.Scanner;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        double number1, number2;
-        String operator;
 
-        Scanner scanner = new Scanner(System.in);
+        char restart = 'Y';
+        do {
+            double number1, number2;
+            String operator;
 
-        System.out.println("Enter the first number:");
-        number1 = scanner.nextDouble();
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the second number:");
-        number2 = scanner.nextDouble();
+            System.out.println("Enter the first number:");
+            number1 = scanner.nextDouble();
 
-        scanner.nextLine();
-        System.out.println("Enter the operator:");
-        operator = scanner.nextLine();
+            System.out.println("Enter the second number:");
+            number2 = scanner.nextDouble();
 
-        switch (operator) {
-            case "+":
-                System.out.println("Output is " + (number1 + number2));
-                break;
-            case "-":
-                System.out.println("Output is " + (number1 - number2));
-                break;
-            case "*":
-                System.out.println("Output is " + (number1 * number2));
-                break;
-            case "/":
-                System.out.println("Output is " + (number1 / number2));
-                break;
-            case "%":
-                System.out.println("Output is " + (number1 % number2));
-                break;
-            default:
-                System.out.println("Invalid operator, Please try again!");
+            scanner.nextLine();
+            System.out.println("Enter the operator:");
+            operator = scanner.nextLine();
+
+            switch (operator) {
+                case "+":
+                    System.out.println("Output is " + (number1 + number2));
+                    break;
+                case "-":
+                    System.out.println("Output is " + (number1 - number2));
+                    break;
+                case "*":
+                    System.out.println("Output is " + (number1 * number2));
+                    break;
+                case "/":
+                    System.out.println("Output is " + (number1 / number2));
+                    break;
+                case "%":
+                    System.out.println("Output is " + (number1 % number2));
+                    break;
+                default:
+                    System.out.println("Invalid operator, Please try again!");
+            }
+            System.out.println("Do you want to constinue ? (Y/N)");
+            restart = scanner.next().charAt(0);
+        }
+        while (restart == 'Y' || restart == 'y');
+        if (restart == 'N' || restart == 'n') {
+            System.out.println("Okay, have a nice day!");
         }
     }
 }
