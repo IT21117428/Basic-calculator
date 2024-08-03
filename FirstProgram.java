d25adfa0dc9b14475aecfe3face48db33b6e6125
kaveeja1999 <87494020+IT21117428@users.noreply.github.com>
2024-08-03 16:48:18 +0530

    create the switch statement for preform which statement is going on

--- FirstProgram.java
+++ FirstProgram.java
@@ -13,7 +13,28 @@ public class FirstProgram {
         System.out.println("Enter the second number:");
         number2 = scanner.nextDouble();
 
+        scanner.nextLine();
         System.out.println("Enter the operator:");
         operator = scanner.nextLine();
+
+        switch (operator) {
+            case "+":
+                System.out.println("Output is " + (number1 + number2));
+                break;
+            case "-":
+                System.out.println("Output is " + (number1 - number2));
+                break;
+            case "*":
+                System.out.println("Output is " + (number1 * number2));
+                break;
+            case "/":
+                System.out.println("Output is " + (number1 / number2));
+                break;
+            case "%":
+                System.out.println("Output is " + (number1 % number2));
+                break;
+            default:
+                System.out.println("Invalid operator, Please try again!");
+        }
     }
 }
