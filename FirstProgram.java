cf704d6a4e781223788d523ed2b2f457985e7b40
kaveeja1999 <87494020+IT21117428@users.noreply.github.com>
2024-08-03 16:32:34 +0530

    Defined the user inputs

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class FirstProgram {
     public static void main(String[] args) {
         double number1, number2;
         String operator;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        number1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number:");
+        number2 = scanner.nextDouble();
+
+        System.out.println("Enter the operator:");
+        operator = scanner.nextLine();
     }
 }
